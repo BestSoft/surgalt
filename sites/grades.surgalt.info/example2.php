fbffb1aab42b9193ee01edc48c8044b2f79e8089
pc1 <pc1@10.0.1.98>
2013-10-06 17:10:38 +0800

    Ерөнхий хэсгүүдээ хэсэгчлэн зурж эхэлж байна удахгүй нийлүүлнэ ...

---
+++ sites/grades.surgalt.info/example2.php
@@ -0,0 +1,61 @@
+<html>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <style>
+        ul
+        {
+            display:none;
+        }
+    </style>
+    <body>
+        <div class="jishee_shuu">
+        <a hre="#">Surgalt</a>&nbsp;&nbsp;<i class="icon-plus icon"></i><br>
+        <ul>
+            <li>                
+                1
+            </li>
+            <li>                
+                2
+            </li>
+            <li>                
+                3
+            </li>
+        </ul>        
+        Surgalt&nbsp;&nbsp;<i class="icon-plus icon"></i><br>
+        <ul>
+            <li>                
+                1
+            </li>
+            <li>                
+                2
+            </li>
+            <li>                
+                3
+            </li>
+        </ul>
+        Surgalt&nbsp;&nbsp;<i class="icon-plus icon"></i><br>
+        <ul>
+            <li>                
+                1
+            </li>
+            <li>                
+                2
+            </li>
+            <li>                
+                3
+            </li>
+        </ul>
+        </div>
+    </body>
+    <script src="js/jquery 1.7.1.js"></script>        
+    <script>        
+        $(document).ready(function(){
+            $(".icon").click(function(){
+                var dooshoo_ursdag = $(this).next().next();
+                dooshoo_ursdag.slideDown();
+                dooshoo_ursdag.siblings("ul").slideUp();
+                $(this).attr("class","icon-minus icon");
+                $(this).siblings("i").attr("class","icon-plus icon");
+            });
+        });
+    </script>
+</html>
