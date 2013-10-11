eef0949ef836336d15cf86dabe05dec64cd42d64
Orgilsuren <o.orgilsuren@yahoo.com>
2013-10-11 17:20:16 +0800

    Update

--- tpl/menu.php
+++ tpl/menu.php
@@ -1,4 +1,45 @@
-<header class="navbar navbar-fixed-top" role="banner">
+<!--<link rel="stylesheet" type="text/css" href="../css/bootstrap-responsive.css">
+<link rel="stylesheet" type="text/css" href="../css/bootstrap-responsive.min.css">
+<link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="../css/style.css">-->
+<div class="navbar navbar-fixed-top">
+    <div class="navbar-inner navbar-default" id="1" style="margin-bottom: 0px;">
+    <a class="brand" href="#">Цахим сургалтын систем</a>
+    <marquee style="margin-top: 0.7%;">Зарлалууд</marquee>
+    </div>
+  <div class="navbar navbar-default" id="2" style="margin-top: 0px;">
+    <div class="navbar-inner">
+    <a class="brand" href="#">Surgalt.INFO</a>
+    <ul class="nav navbar-nav">
+        <li><a href="?host=lesson.surgalt.info"><span class="ext mn"></span><span class="glyphicon-book"></span>Хичээл</a></li>
+        <li><a href="?host=test.surgalt.info"><span class="ext net"></span>Шалгалт</a></li>
+        <li><a href="?host=grades.surgalt.info"><span class="ext mobi"></span>Журнал</a></li>
+        <li><a href="?host=calendar.surgalt.info"><span class="ext co"></span>Цаглабар</a></li>
+        <li><a href="?host=credit.surgalt.info"><span class="ext tv"></span>Цагийн тооцоо</a></li>
+      </ul>
+    <button id="btn" class="btn btn-default" style="position: absolute; margin-left: 35%; margin-top: 0.5%"><span class="glyphicon-book"></span>Sw10d219@csms.edu.mn</button>
+    <div id="Settings" style="display: none; width: 450px;">
+        <div>
+            <img src="../img/avatar.jpg" class="img-polaroid"><br>
+        <i><b>Овог нэр:</b> О.Оргилсүрэн</i><br>
+        <i><b>Е-мэйл:</b> sw10d219@csms.edu.mn</i><br>
+        <i><b>Оюутны код:</b> D.SW10D219</i><br>
+        <i><b>Рег.дугаар:</b> ТА92021918</i><br>
+        <i><b>Мэргэжлийн индэкс:</b> D524000</i><br>
+        <i><b>Кредит:</b> 118</i><br>
+        <i><b>ҮГД:</b> 2.64</i>
+        <br>
+        <button class="btn btn-default" style="float: right;">
+            Гарах
+        </button>
+        </div>
+    </div>
+    </div>
+  </div>
+</div>
+    
+<!--<header class="navbar navbar-fixed-top" role="banner">
     <div class="container">     
     <div class="navbar-header">
       <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
@@ -8,18 +49,18 @@
         <span class="icon-bar"></span>
       </button>
         <a href="./" class="navbar-brand">Цахим сургалтын систем</a>
-        <ul id="text_1">
+        <!--<ul id="text_1">
             <li class="algaboldog">1Энд сургалттай холбоотой зарууд байрлах бөгөөд, оюутан багш нарт харагдах юм. Coming Soon!</li>
             <li class="algaboldog">2Энд сургалттай холбоотой зарууд байрлах бөгөөд, оюутан багш нарт харагдах юм. Coming Soon!</li>
             <li class="algaboldog">3Энд сургалттай холбоотой зарууд байрлах бөгөөд, оюутан багш нарт харагдах юм. Coming Soon!</li>
 
-        </ul>
-        <i id="count"></i>
+        </ul>-->
+        <!--<i id="count"></i>
         <marquee style="position: absolute; direction: ltr; padding-top: 15px;">
             Энд сургалттай холбоотой зарууд байрлах бөгөөд, оюутан багш нарт харагдах юм. Coming Soon!
         </marquee>
     </div>
-        <button id="btn" class="btn btn-default" style="position: absolute; margin-left: 60%; margin-top: 1%"><span class="glyphicon glyphicon-star"></span>Sw10d219@csms.edu.mn</button>
+        <button id="btn" class="btn btn-default" style="position: absolute; margin-left: 60%; margin-top: 1%"><span class="glyphicon-book"></span>Sw10d219@csms.edu.mn</button>
   </div>
 <div id="Settings" style="display: none; width: 500px;">
     <div>
@@ -53,8 +94,9 @@
         <li><a href="?host=test.surgalt.info"><span class="ext net"></span>Шалгалт</a></li>
         <li><a href="?host=grades.surgalt.info"><span class="ext mobi"></span>Журнал</a></li>
         <li><a href="?host=calendar.surgalt.info"><span class="ext co"></span>Цаглабар</a></li>
-        <li><a href="?host=credit.surgalt.info"><span class="ext tv"></span>Цагийн тооцоо</a></li>
+        <li><a href="?host=credit.surgalt.info"><span class="ext tv"></span>Цагийн тооцоо<i class="icon-remove"></i></a></li>
       </ul>
     </nav>
   </div>
-</header>
+        
+</header>-->
