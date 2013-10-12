5ca814d0fd467d195c077aca40995a7c51153604
Orgilsuren <o.orgilsuren@yahoo.com>
2013-10-12 18:31:00 +0800

    Засвар 2

--- tpl/menu.php
+++ tpl/menu.php
@@ -1,18 +1,18 @@
 <div class="navbar navbar-fixed-top">
     <div class="navbar-inner navbar-default" id="1" style="margin-bottom: 0px;">
-    <a class="brand" href="#">Цахим сургалтын систем</a>
+    <a class="brand" href="<?php echo BASE_URL; ?>">Цахим сургалтын систем</a>
     </div>
   <div class="navbar navbar-default" id="2" style="margin-top: 0px;">
     <div class="navbar-inner">
-    <a class="brand" href="#">Surgalt.INFO</a>
+        <a class="brand" href="<?php echo BASE_URL; ?>">Surgalt.INFO</a>
     <ul class="nav navbar-nav">
-        <li><a href="?host=lesson.surgalt.info"><span class="ext mn"></span><span class="glyphicon-book"></span>Хичээл</a></li>
-        <li><a href="?host=test.surgalt.info"><span class="ext net"></span>Шалгалт</a></li>
-        <li><a href="?host=grades.surgalt.info"><span class="ext mobi"></span>Журнал</a></li>
-        <li><a href="?host=calendar.surgalt.info"><span class="ext co"></span>Цаглабар</a></li>
-        <li><a href="?host=credit.surgalt.info"><span class="ext tv"></span>Цагийн тооцоо</a></li>
+        <li><a href="?host=lesson.surgalt.info"><span class="ext mn"></span><i class="icon icon-book"></i> Хичээл</a></li>
+        <li><a href="?host=test.surgalt.info"><span class="ext net"></span><i class="icon icon-file"></i> Шалгалт</a></li>
+        <li><a href="?host=grades.surgalt.info"><span class="ext mobi"></span><i class="icon icon-ok"></i> Журнал</a></li>
+        <li><a href="?host=calendar.surgalt.info"><span class="ext co"></span><i class="icon icon-calendar"></i> Цаглабар</a></li>
+        <li><a href="?host=credit.surgalt.info"><span class="ext tv"></span><i class="icon icon-time"></i> Цагийн тооцоо</a></li>
       </ul>
-    <button id="btn" class="btn btn-default" style="position: absolute; margin-left: 35%; margin-top: 0.5%"><span class="glyphicon-book"></span>Sw10d219@csms.edu.mn</button>
+    <button id="btn" class="btn btn-default" style="position: absolute; margin-left: 30%; margin-top: 0.5%"><i class="icon icon-user"></i>Sw10d219@csms.edu.mn</button>
     <div id="Settings" style="display: none; width: 450px;">
         <div>
             <img src="../img/avatar.jpg" class="img-polaroid"><br>
