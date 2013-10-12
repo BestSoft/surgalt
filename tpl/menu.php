935462a7122435afc94cef6ecf9bcd9b34c6387a
Orgilsuren <o.orgilsuren@yahoo.com>
2013-10-12 18:50:14 +0800

    Засвар 3

--- tpl/menu.php
+++ tpl/menu.php
@@ -14,20 +14,7 @@
       </ul>
     <button id="btn" class="btn btn-default" style="position: absolute; margin-left: 30%; margin-top: 0.5%"><i class="icon icon-user"></i>Sw10d219@csms.edu.mn</button>
     <div id="Settings" style="display: none; width: 450px;">
-        <div>
-            <img src="../img/avatar.jpg" class="img-polaroid"><br>
-        <i><b>Овог нэр:</b> О.Оргилсүрэн</i><br>
-        <i><b>Е-мэйл:</b> sw10d219@csms.edu.mn</i><br>
-        <i><b>Оюутны код:</b> D.SW10D219</i><br>
-        <i><b>Рег.дугаар:</b> ТА92021918</i><br>
-        <i><b>Мэргэжлийн индэкс:</b> D524000</i><br>
-        <i><b>Кредит:</b> 118</i><br>
-        <i><b>ҮГД:</b> 2.64</i>
-        <br>
-        <button class="btn btn-default" style="float: right;">
-            Гарах
-        </button>
-        </div>
+        <?php include_once 'userSettings.php'; ?>
     </div>
     </div>
   </div>
--- tpl/userSettings.php
+++ tpl/userSettings.php
@@ -1,9 +1,12 @@
+<?php
+$user = User::getInstance();
+?>
+
 <div>
-    <img src="../img/avatar.jpg" class="img-polaroid"><br>
-    <i><b>Овог нэр:</b> О.Оргилсүрэн</i><br>
-    <i><b>Е-мэйл:</b> sw10d219@csms.edu.mn</i><br>
-    <i><b>Оюутны код:</b> D.SW10D219</i><br>
-    <i><b>Рег.дугаар:</b> ТА92021918</i><br>
+    <img src="<?php echo BASE_URL; ?>/img/avatar.jpg" class="img-polaroid" style="height: 100px;"><br>
+    <i><b>Овог нэр:</b> <?php echo $user->getUsrLnm()." ".$user->getUsrNm() ?></i><br>
+    <i><b>Е-мэйл:</b> <?php echo $user->getEmail() ?></i><br>
+    <i><b>Оюутны код:</b> <?php echo $user->getUsrCd() ?></i><br>
     <i><b>Мэргэжлийн индэкс:</b> D524000</i><br>
     <i><b>Кредит:</b> 118</i><br>
     <i><b>ҮГД:</b> 2.64</i>
