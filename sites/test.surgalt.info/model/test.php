29b4657ecf4b6819a8ba0add8208f9f6051a03ff
Zolboo <Zolboo@10.0.1.172>
2013-10-16 16:57:33 +0800

    LessonModel дотор хэрэглэгчийн хичээлийг татах функцийг бичлээ.

--- sites/test.surgalt.info/model/test.php
+++ sites/test.surgalt.info/model/test.php
@@ -2,9 +2,9 @@
 
 class TestModel {
 
-    public static function getUserTest() {
+    public static function getUserTest($usrId) {
         $db = DataBase::getInstance();
-        $sql = 'SELECT b.LsnNm, c.TstNm FROM userlesson AS a LEFT JOIN lesson AS b ON a.LsnID = b.LsnID LEFT JOIN test AS c ON a.LsnID = c.LsnID WHERE a.UsrID = ' . User::getInstance()->getUsrID();
+        $sql = 'SELECT c.* FROM userlesson AS a LEFT JOIN test AS c ON a.LsnID = c.LsnID WHERE a.UsrID = ' . $usrId;
         $query = $db->query($sql);
         $result = array();
         while($test = $query->fetch_assoc()){
--- sites/test.surgalt.info/view/list.php
+++ sites/test.surgalt.info/view/list.php
@@ -4,13 +4,19 @@ require_once PATH_BASE . '/sites/lesson.surgalt.info/model/lesson.php';
 require_once PATH_BASE . '/sites/' . HOSTNAME . '/model/test.php';
 
 class ListView {
-    private $tests;
+    private $title;
+    private $lessons;
     
     function display() {
         if (User::getInstance()->isGuest()) {
             echo 'Хандах эрхгүй байна.';
         } else {
-            $this->tests = LessonModel::getLessonlist(User::getInstance()->getUsrId());
+            $this->title = "Хичээлийн жагсаалт";
+            $this->lessons = LessonModel::getLessonlist(User::getInstance()->getUsrId());
+            $tests = TestModel::getUserTest(User::getInstance()->getUsrId());
+            foreach($tests as $test){
+                $this->tests[$test['LsnID']][] = $test;
+            }
             include_once 'list.html.php';
         }
     }
